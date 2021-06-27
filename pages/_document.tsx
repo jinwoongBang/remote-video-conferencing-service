10cd96f4eb4b2b6e0a0d1ab33ddfd19247a0fe33
jinwoong-bang <jwbang@malgn.com>
2021-06-27 15:16:50 +0900

    feature  : _documents.js 에 getInitialProps 함수 주석처리하여 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,14 +3,24 @@ import Document, { Head, Main, NextScript } from 'next/document';
 import React from 'react';
 
 export default class MyDocument extends Document {
+
+  /**
+   * @description 최초 데이터 fetch 시 사용해도 괜찮을 듯?
+   * @returns 
+   */
+  // static async getInitialProps(ctx) {
+  //   const initialProps = await Document.getInitialProps(ctx);
+  //   return { ...initialProps };
+  // }
+
   render() {
     return (
       <html>
         <Head>
-          <meta
+          {/* <meta
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
-          />
+          /> */}
           {/* 여기에 폰트 임포팅, 여기에 공통 CSS e.g.reset-css/common.css */}
         </Head>
         <body>
