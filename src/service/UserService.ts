1379c6a4234bb59d6770d5ff64b742dd8869764f
HanSangDuk <ramsang0212@gmail.com>
2021-07-31 15:02:59 +0900

    user registration working

--- src/service/UserService.ts
+++ src/service/UserService.ts
@@ -23,6 +23,26 @@ class UserService {
 
     return userList;
   }
+
+
+  async insertUser() {
+    const conn = await connectionPool.getConnection();
+    const rows = await conn.query('SELECT * FROM TB_USER'); // 쿼리 실행
+    const row = rows[0];
+
+    const user = new UserVO({
+      userName: row.AUTHORITIES,
+      userId: row.USER_ID,
+      userPassword: row.PASSWORD,
+    });
+
+    let userList: UserVO[] = [user];
+    userList = JSON.parse(JSON.stringify(userList));
+
+    await conn.release();
+
+    return userList;
+  }
 }
 
 export default new UserService();
