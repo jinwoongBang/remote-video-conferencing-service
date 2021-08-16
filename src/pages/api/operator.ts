b6a11c529460bc53c06fca28553c4900e0c0c28d
jinwoong-bang <jwbang@malgn.com>
2021-08-16 15:09:11 +0900

    feature : 운영자 관련 API 추가

---
+++ src/pages/api/operator.ts
@@ -0,0 +1,65 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+import OTAResponse from 'src/common/framework/OTAResponse';
+import { PreferenceVO, User } from 'src/vo';
+
+export interface OperatorResponseEntity {
+  method: string;
+}
+
+export type OperatorPostParam = {
+  param: User;
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<OTAResponse<OperatorResponseEntity>>,
+) {
+  const { method } = req;
+
+  switch (method) {
+    case 'GET':
+      doGet(req, res);
+      break;
+    case 'POST':
+      doPost(req, res);
+      break;
+    case 'PUT':
+      doPut(req, res);
+      break;
+    default:
+      throw new Error('지정된 Method API 가 존재하지 않습니다.');
+  }
+}
+
+function doGet(
+  req: NextApiRequest,
+  res: NextApiResponse<OTAResponse<OperatorResponseEntity>>,
+) {
+  const query = req.query;
+  console.log({ query });
+  const response = new OTAResponse<OperatorResponseEntity>();
+  const result: OperatorResponseEntity[] = [];
+  response.result = result;
+  res.status(200).json(response);
+}
+
+function doPost(
+  req: NextApiRequest,
+  res: NextApiResponse<OTAResponse<OperatorResponseEntity>>,
+) {
+  const response = new OTAResponse<OperatorResponseEntity>();
+  const result: OperatorResponseEntity[] = [];
+  response.result = result;
+  res.status(200).json(response);
+}
+
+async function doPut(
+  req: NextApiRequest,
+  res: NextApiResponse<OTAResponse<OperatorResponseEntity>>,
+) {
+  const response = new OTAResponse<OperatorResponseEntity>();
+  const result: OperatorResponseEntity[] = [];
+  response.result = result;
+  res.status(200).json(response);
+}
--- src/service/OperatorService.ts
+++ src/service/OperatorService.ts
@@ -2,15 +2,43 @@ import OTAService from 'src/service/OTAService';
 import { User } from 'src/vo';
 
 class OperatorService extends OTAService {
-  async insertOperator(user: User) {
+  async insertOperator(param: User) {
     let conn;
+    let resultCount = 0;
+
     try {
       conn = await this.getConnection();
-      conn.query(``);
+      const queryResult = await conn.query(`
+        INSERT INTO TB_USER(
+            ROOM_ID,
+            EVENT_ID,
+            AUTHORITIES,
+            USER_ID,
+            PASSWORD,
+            NAME,
+            PHONE_NUMBER,
+            EMAIL,
+            TYPE
+        ) VALUES (
+            0,
+            0,
+            ${param.AUTHORITIES},
+            ${param.USER_ID},
+            ${param.PASSWORD},
+            ${param.NAME},
+            ${param.PHONE_NUMBER},
+            ${param.EMAIL},
+            2
+        )
+      `);
+      resultCount = queryResult.affectedRows;
     } catch (error) {
+      console.error(error);
     } finally {
       conn && (await conn.release());
     }
+
+    return resultCount;
   }
 }
 
