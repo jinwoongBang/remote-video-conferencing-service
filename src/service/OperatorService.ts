b442b83978546c52397e0b2f0daf7cf8eed6aed1
jinwoong-bang <jwbang@malgn.com>
2021-08-16 14:50:42 +0900

    feature : db 기본 구조 추가

--- src/service/OperatorService.ts
+++ src/service/OperatorService.ts
@@ -1,7 +1,17 @@
 import OTAService from 'src/service/OTAService';
+import { User } from 'src/vo';
 
 class OperatorService extends OTAService {
-  async insertOperator() {}
+  async insertOperator(user: User) {
+    let conn;
+    try {
+      conn = await this.getConnection();
+      conn.query(``);
+    } catch (error) {
+    } finally {
+      conn && (await conn.release());
+    }
+  }
 }
 
 export default new OperatorService();
