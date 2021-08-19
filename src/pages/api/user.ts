388385436e87ad12581967d25acadcf2867b7951
sangdukHan <ramsang0212@hoogi.com>
2021-08-19 13:43:01 +0900

    feat: user select ing ~

--- src/pages/api/user.ts
+++ src/pages/api/user.ts
@@ -1,9 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { KeyboardReturnOutlined } from '@material-ui/icons';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { atom, atomFamily, selector, selectorFamily } from 'recoil';
 import OTAResponse from 'src/common/framework/OTAResponse';
 import UserService from 'src/service/UserService';
+import { SiteInformationProps } from 'src/store/preference';
 import { reqUser } from 'src/vo';
+import { UserSearch } from 'src/store/user';
 import UserVO from '../../vo/UserVO';
 
 export type Data = {
@@ -13,43 +16,65 @@ export type Data = {
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<OTAResponse<Data>>,
+  res: NextApiResponse<OTAResponse<UserVO>>,
 ) {
-  const { method, body } = req;
+  const { method, body, query } = req;
+  // console.log(
+  //   'api user req:: ' + method + '  // body:: ' + JSON.stringify(body),
+  // );
   console.log(
-    'api user req:: ' + method + '  // body:: ' + JSON.stringify(body),
+    'api user method[' +
+      method +
+      ']:: ' +
+      JSON.stringify(query) +
+      '  // body:: ' +
+      JSON.stringify(body),
   );
+
+  const querys = query as UserSearch;
+
+  console.log('api user query::', querys);
 
   const reqUser = body as reqUser;
   console.log('// reqUser:: ' + JSON.stringify(reqUser));
 
-  const checkUser = await UserService.selectUserIdCheck(reqUser.userId ?? '');
+  const seminarResponse = new OTAResponse<UserVO>();
 
-  const seminarResponse = new OTAResponse<Data>();
-
-  if (!checkUser) {
-    console.log('api user:: exist user: ');
-    seminarResponse.success = false;
-    res.send(seminarResponse);
-    return;
-  }
-
-  const data = await UserService.insertUser(reqUser);
-
-  console.log('api user:: insert data: ' + data);
+  // if(true){
+  //   seminarResponse.success = false;
+  //   res.send(seminarResponse);
+  //   return;
+  // }
 
   const result: Data[] = [
     { id: 20, name: 'before1' },
     { id: 20, name: 'before2' },
     { id: 20, name: 'before5' },
   ];
-  seminarResponse.result = result;
 
   switch (method) {
     case 'GET':
+      seminarResponse.result = await UserService.selectUserList(querys.userId);
       res.status(200).json(seminarResponse);
       break;
     case 'POST':
+      const reqUser = body as reqUser;
+      console.log('POST // reqUser:: ' + JSON.stringify(reqUser));
+
+      const checkUser = await UserService.selectUserIdCheck(
+        reqUser.userId ?? '',
+      );
+
+      if (!checkUser) {
+        console.log('POST api user:: exist user: ');
+        seminarResponse.success = false;
+        res.send(seminarResponse);
+        return;
+      }
+
+      const data = await UserService.insertUser(reqUser);
+
+      console.log('POST api user:: insert data: ' + data);
       res.status(200).json(seminarResponse);
       break;
     default:
--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from 'react';
-
+import React, { useEffect, useState, useCallback, useMemo } from 'react';
 /**
  * next
  */
@@ -10,13 +9,17 @@ import Counter from 'src/components/Counter';
 /**
  * recoil
  */
-import { useRecoilState, useSetRecoilState } from 'recoil';
+import {
+  useRecoilState,
+  useSetRecoilState,
+  useRecoilStateLoadable,
+} from 'recoil';
 
 /**
  * store
  */
 import { authState, counterState } from 'src/store';
-import UserVO from 'src//vo/UserVO';
+import UserVO, { User } from 'src//vo/UserVO';
 import { DataGrid, GridRowsProp, GridColDef } from '@material-ui/data-grid';
 
 /**
@@ -24,98 +27,226 @@ import { DataGrid, GridRowsProp, GridColDef } from '@material-ui/data-grid';
  */
 import UserService from 'src/service/UserService';
 
-
 /**
  * component
  */
 import ApoLayout from 'src/components/AppLayout';
 
+/**
+ *  Material UI
+ */
+import {
+  Button,
+  Checkbox,
+  Divider,
+  FormControlLabel,
+  FormGroup,
+  Grid,
+  TextField,
+  Typography,
+} from '@material-ui/core';
+import { makeStyles, Theme } from '@material-ui/core/styles';
+import {
+  Cancel,
+  Copyright,
+  Create,
+  HowToReg,
+  Mail,
+  Person,
+  PhoneAndroid,
+  SecurityRounded,
+  SecuritySharp,
+  VpnKey,
+} from '@material-ui/icons';
 
-const columns: GridColDef[] = [
-  { field: 'regDate', headerName: '가입일', width: 150 },
-  { field: 'eventCode', headerName: '이벤트(code)', width: 150 },
-  { field: 'status', headerName: '상태', width: 150 },
-  { field: 'userId', headerName: '아이디', width: 150 },
-  { field: 'pw', headerName: '비밀번호', width: 150 },
-  { field: 'userName', headerName: '회원이름', width: 150 },
-  { field: 'PhoneNumber', headerName: '연락처', width: 150 },
-  { field: 'email', headerName: '이메일', width: 150 },
-  { field: 'loginInfo', headerName: '로그인정보', width: 150 },
-  { field: 'management', headerName: '관리', width: 150 },
-];
+import { useRecoilValueLoadable } from 'recoil';
+import {
+  userSearchState,
+  forceReloadUserListState,
+  userListSelector,
+} from 'src/store/user';
 
-function User({ userList }: InferGetStaticPropsType<typeof getStaticProps>) {
-  console.log('getStaticProps() :: no hooks');
+const useStyles = makeStyles((theme: Theme) => ({
+  inputLabelContainer: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    padding: '15px',
+  },
+  inputContainer: {
+    padding: '15px',
+  },
+  buttonContaier: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    padding: '15px',
+    gap: '10px',
+  },
+  modifyButton: {
+    width: '200px',
+  },
+  divider: {
+    padding: '15px',
+  },
+}));
+
+function UserView({
+  userList,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  console.log('user getStaticProps() :: no hooks', userList);
   const [auth, setAuth] = useRecoilState(authState);
+  const classes = useStyles();
+  // todo 0724 데이터가 들어가게 수정 필요 !!
 
-  // todo 0724 데이터가 들어가게 수정 필요 !! 
-  const rows: GridRowsProp = [
-    { 
-      id: 1, 
-      regDate: '2021 test', 
-      eventCode: 'event Code test', 
-      status: 'status test',
-      userId: 'id test',
-      pw: 'pw test',
-      userName: 'userName test',
-      PhoneNumber: 'PhoneNumber test',
-      email: 'email test', 
-      loginInfo: 'loginInfo test', 
-      management: 'management test', 
-    },
-    { 
-      id: 2, 
-      regDate: '2021 test2', 
-      eventCode: 'event Code test', 
-      status: 'status test',
-      userId: 'id test',
-      pw: 'pw test',
-      userName: 'userName test',
-      PhoneNumber: 'PhoneNumber test',
-      email: 'email test', 
-      loginInfo: 'loginInfo test', 
-      management: 'management test', 
-    },
-    { 
-      id: 3, 
-      regDate: '2021 test3', 
-      eventCode: 'event Code test', 
-      status: 'status test',
-      userId: 'id test',
-      pw: 'pw test',
-      userName: 'userName test',
-      PhoneNumber: 'PhoneNumber test',
-      email: 'email test', 
-      loginInfo: 'loginInfo test', 
-      management: 'management test', 
-    },
+  // const boardList = useRecoilValueLoadable(boardListSelector);
+  const [users, reload] = useRecoilStateLoadable(userListSelector);
+  const [force, setForce] = useRecoilState(forceReloadUserListState);
+  const [userSearch, setUserSearchState] = useRecoilState(userSearchState);
+
+  console.log('click boardSearchState: ' + userSearch.userId);
+  console.log('click users: ', users?.contents);
+  console.log('users?.state: ', users?.state);
+  let rows: GridRowsProp = useMemo(() => {
+    return users?.state === 'hasValue'
+      ? users.contents.result.map((x, index) => ({
+          id: x.ID,
+          regDate: x.DATE_OF_CREATED,
+          eventCode: x.EVENT_ID,
+          status: x.STATUS,
+          userId: x.USER_ID,
+          pw: x.PASSWORD,
+          userName: x.NAME,
+          PhoneNumber: x.PHONE_NUMBER,
+          email: x.EMAIL,
+          loginInfo: x.STATUS,
+          management: 'management?? 뭐  넣ㅓㅏ', //todo 0810 회원쪽도 어떻게 넣을지 봐야할 듯
+        }))!
+      : [];
+  }, []);
+
+  if (users?.state === 'hasValue') {
+    console.log('click users: ', users.contents.result[0]);
+    rows = users.contents.result.map((x, index) => ({
+      id: x.ID,
+      regDate: x.DATE_OF_CREATED,
+      eventCode: x.EVENT_ID,
+      status: x.STATUS,
+      userId: x.USER_ID,
+      pw: x.PASSWORD,
+      userName: x.NAME,
+      PhoneNumber: x.PHONE_NUMBER,
+      email: x.EMAIL,
+      loginInfo: x.STATUS,
+      management: 'management?? 뭐  넣ㅓㅏ', //todo 0810 회원쪽도 어떻게 넣을지 봐야할 듯
+    }));
+  }
+
+  const columns: GridColDef[] = [
+    { field: 'regDate', headerName: '가입일', width: 150 },
+    { field: 'eventCode', headerName: '이벤트(code)', width: 150 },
+    { field: 'status', headerName: '상태', width: 150 },
+    { field: 'userId', headerName: '아이디', width: 150 },
+    { field: 'pw', headerName: '비밀번호', width: 150 },
+    { field: 'userName', headerName: '회원이름', width: 150 },
+    { field: 'PhoneNumber', headerName: '연락처', width: 150 },
+    { field: 'email', headerName: '이메일', width: 150 },
+    { field: 'loginInfo', headerName: '로그인정보', width: 150 },
+    { field: 'management', headerName: '관리', width: 150 },
   ];
-  
+
+  console.log('row222row222: ', rows);
+  const handleSubmitOperator = useCallback(() => {
+    setUserSearchState({
+      userId: 'ram',
+    });
+    reload(0 as any);
+
+    console.log('click row222row222: ', users.contents.result);
+  }, []);
+  const handleSubmitResetOperator = useCallback(() => {
+    setUserSearchState({
+      userId: '',
+    });
+    reload(0 as any);
+
+    console.log('reset click row222row222: ', users.contents.result);
+  }, []);
+
+  const bkbk = userList[0];
 
   useEffect(() => {
     const user = userList[0] || null;
     setAuth((currVal) => ({ ...currVal, user }));
-    console.log('getStaticProps() :: useEffect (mount)');
+    console.log('getStaticProps() :: useEffect (mount)', userList);
     return () => {
-      console.log('getStaticProps() :: useEffect (unmount)');
+      console.log('getStaticProps() :: useEffect (unmount)', userList[0]);
     };
   }, []);
 
-  return (
-    <ApoLayout>
-      <div>
-        <div>
-          <h1>User</h1>
-          <h2>{auth.user && auth.user.userName}</h2>
-        </div>
-        
-        <div style={{ height: 300, width: '100%' }}>
-          <DataGrid rows={rows} columns={columns} />
-        </div>
+  switch (users?.state) {
+    case 'hasValue':
+      return (
+        <ApoLayout>
+          <div>
+            <div>
+              <h1>User </h1>
+              <h2>{auth.user && auth.user.userName}</h2>
+            </div>
 
-      </div>
-    </ApoLayout>
-  );
+            <div style={{ height: 300, width: '100%' }}>
+              <DataGrid rows={rows} columns={columns} />
+            </div>
+            <Grid item xs={12} className={classes.buttonContaier}>
+              <Button
+                variant="contained"
+                color="primary"
+                size="large"
+                className={classes.modifyButton}
+                startIcon={<Create />}
+                onClick={handleSubmitOperator}
+              ></Button>
+              <Button
+                variant="contained"
+                color="primary"
+                size="large"
+                className={classes.modifyButton}
+                startIcon={<Person />}
+                onClick={handleSubmitResetOperator}
+              ></Button>
+            </Grid>
+          </div>
+        </ApoLayout>
+      );
+    case 'loading':
+      return (
+        <ApoLayout>
+          <div>
+            <div>
+              <h1>User </h1>
+              <h2>{auth.user && auth.user.userName}</h2>
+            </div>
+
+            <div style={{ height: 300, width: '100%' }}>
+              <div>Loading...</div>
+            </div>
+
+            <div>
+              <Button
+                variant="contained"
+                color="primary"
+                size="large"
+                className={classes.modifyButton}
+                startIcon={<Create />}
+                onClick={handleSubmitOperator}
+              >
+                등록
+              </Button>
+            </div>
+          </div>
+        </ApoLayout>
+      );
+    case 'hasError':
+      throw <div>Error...</div>;
+  }
 }
 
 // This function gets called at build time
@@ -131,4 +262,4 @@ export const getStaticProps: GetStaticProps<{ userList: UserVO[] }> = async ({
   };
 };
 
-export default User;
+export default UserView;
