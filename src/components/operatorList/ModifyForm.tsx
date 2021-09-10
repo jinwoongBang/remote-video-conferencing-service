bd71c346545f376dae358b3a43e7f8d0950403db
jinwoong-bang <jwbang@malgn.com>
2021-09-10 13:35:41 +0900

    feature : 운영자 수정 form 유효성 검증 로직 추가

--- src/components/operatorList/ModifyForm.tsx
+++ src/components/operatorList/ModifyForm.tsx
@@ -282,7 +282,15 @@ function ModifyForm({ operator, onOpen }: ModifyFormProps) {
               fullWidth
               id="standard-required"
               placeholder="운영자 이름 입력해주세요."
-              {...register(FormKey.NAME)}
+              helperText={errors.NAME?.message}
+              error={Boolean(errors.NAME)}
+              {...register(FormKey.NAME, {
+                required: '아이디를 입력해주세요.',
+                maxLength: {
+                  value: 3,
+                  message: '4자 이하로 입력해주세요.',
+                },
+              })}
             />
           </Grid>
           {/*  */}
@@ -301,9 +309,22 @@ function ModifyForm({ operator, onOpen }: ModifyFormProps) {
           <Grid item xs={4} className={classes.inputContainer}>
             <TextField
               fullWidth
+              type="password"
               id="standard-required"
               placeholder="비밀번호를 입력해주세요."
-              {...register(FormKey.PASSWORD)}
+              helperText={errors.PASSWORD?.message}
+              error={Boolean(errors.PASSWORD)}
+              {...register(FormKey.PASSWORD, {
+                required: '아이디를 입력해주세요.',
+                minLength: {
+                  value: 4,
+                  message: '4 ~ 16자로 입력해주세요.',
+                },
+                maxLength: {
+                  value: 16,
+                  message: '4 ~ 16자로 입력해주세요.',
+                },
+              })}
             />
           </Grid>
           {/*  */}
@@ -315,6 +336,7 @@ function ModifyForm({ operator, onOpen }: ModifyFormProps) {
               size="large"
               startIcon={<EnhancedEncryption />}
               disableRipple
+              type="password"
             >
               비밀번호 확인
             </Button>
@@ -324,7 +346,10 @@ function ModifyForm({ operator, onOpen }: ModifyFormProps) {
               fullWidth
               id="standard-required"
               placeholder="비밀번호를 입력해주세요."
-              {...register(FormKey.PASSWORD_CONFIRM)}
+              {...register(FormKey.PASSWORD_CONFIRM, {
+                required: true,
+                maxLength: 10,
+              })}
             />
           </Grid>
           {/*  */}
